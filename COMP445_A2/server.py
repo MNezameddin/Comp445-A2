60eab77278d272b5f1f596dd39fb94d04b30c587
Omarhammoud <omar_hammoud96@hotmail.com>
2021-03-03 17:27:13 -0500

    Update server.py

--- COMP445_A2/server.py
+++ COMP445_A2/server.py
@@ -11,7 +11,7 @@ class Server:
         print("If you would like to input a Host to connect to, please enter it below. You can press \"Enter\" to use default values.")
         host = input()
         if host == '':
-            host = "localhost"
+            host = "Omars-MacBook-Pro.local"
 
         print("If you would like to input a port to bind to, please enter it below. You can press \"Enter\" to use default values.")
         port = input()
@@ -29,57 +29,42 @@ class Server:
         self.potential_errors = list()
         self.outbox = dict()
         self.messages = dict()
+        
 
     def run(self):
         print("Server running...")
-        try:
-            while True:
-                r_reads, r_writes, r_errors= select.select(self.potential_reads, self.potential_writes, self.potential_errors)
-                for r in r_reads:
+    
+        while True:
+            r_reads, r_writes, r_errors= select.select(self.potential_reads, self.potential_writes, self.potential_errors)
+            for r in r_reads:
                     #server socket accepting clients
-                    if r is self.server_socket:
-                        client_socket, addr = r.accept()
-                        client_socket.setblocking(False)
-                        print("New client @ {}".format(addr))
-                        self.outbox[client_socket] = list()
-                        self.messages[client_socket] = str()
-                        self.potential_reads.append(client_socket)
+                if r is self.server_socket:
+                    client_socket, addr = r.accept()
+                    client_socket.setblocking(False)
+                    print("New client @ {}".format(addr))
+                    self.outbox[client_socket] = list()
+                    self.messages[client_socket] = str()
+                    self.potential_reads.append(client_socket)
+                    
                     # reading from clients
+                else:
+                    data = r.recv(self.read_size).decode('utf-8')
+                    if data:
+                        print("Received data: {}".format(data))
+                        self.messages[r] += data
+                        self._parse_message(r)
+                        for x in self.potential_reads[1:]:
+                            x.sendall(data.encode())
+                            print(x)
                     else:
-                        data = r.recv(self.read_size).decode('utf-8')
-                        if data:
-                            print("Received data: {}".format(data))
-                            self.messages[r] += data
-                            print(self.messages[r])
-                            self._parse_message(r)
-                            print(self._parse_message(r))
-                            print("Sending message to client: {}".format(data.encode()))
-                            r.send(data.encode())
-                        else:
-                            print("Removing client socket from watchlists")
-                            self.potential_reads.remove(r)
-                            if r is self.outbox:
-                                del self.outbox[r]
-                            if r is self.messages:
-                                del self.messages[r]
-                            print("Closed a client socket: {}".format(r))
-                            r.close()
-                for w in r_writes:
-                    msg = self.outbox[w]
-                    if len(msg):
-                        print("Sending message to client: {}".format(w))
-                        w.send(msg.encode())
-                        self._clear_outbox(w)
-                for err in r_errors:
-                    self.potential_reads.remove(err)
-                    if err is self.outbox:
-                        del self.outbox[err]
-                    if err is self.messages:
-                        del self.messages[err]
-        except KeyboardInterrupt:
-            print("\nServer interrupted, closing socket connections")
-        except:
-            pass
+                        print("Removing client socket from watchlists")
+                        self.potential_reads.remove(r)
+                        if r is self.outbox:
+                            del self.outbox[r]
+                        if r is self.messages:
+                            del self.messages[r]
+                        print("Closed a client socket: {}".format(r))
+                        r.close()
         self.close()
 
     def _clear_outbox(self, client_socket):
